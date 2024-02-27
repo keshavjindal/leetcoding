b4f3af29045031d623003a592f71aac65397284b
keshavjindal <52865096+keshavjindal@users.noreply.github.com>
2024-02-27 15:46:56 -0500

    Time: 0 ms (100.00%) | Memory: 42.1 MB (8.74%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,5 +1,37 @@
 class Solution {
-    public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+    public void sortColors(int[] arr) {
+        /*
+            00000 1111?????2222
+                  j.  i.  k 
+            
+            i -> unknown
+            j -> pehle 1 pe
+            k -> pehle 2 pe
+        
+        */
+        int i = 0;
+        int j = 0;
+        int k = arr.length - 1;
+        
+        while(i <= k){
+            if(arr[i] == 1){
+                i++;
+            }
+            else if(arr[i] == 0){
+                swap(arr , i , j);
+                i++;
+                j++;
+            }
+            else if(arr[i] == 2){
+                swap(arr , i , k);
+                k--;
+            }
+        }
+    }
+    
+    public void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
