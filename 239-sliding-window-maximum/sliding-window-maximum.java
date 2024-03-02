04092dfdf65853f7df89a6abe5fe9121ef30c91b
keshavjindal <52865096+keshavjindal@users.noreply.github.com>
2024-03-01 23:51:25 -0500

    Time: 27 ms (94.90%) | Memory: 59.6 MB (36.54%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,47 +1,33 @@
 class Solution {
     public int[] maxSlidingWindow(int[] arr, int k) {
-        /*
-        divide the array into k-k size boxes
-                    
-                    0 1  2    3 4 5
-                    1 3 -1   -3 5 3    6 7 
-          leftmax   1 3 3    -3 5 5    6 7 
-         rightmax   3 3-1     5 5 3    7 7   
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
         
-            ans -> 3 3 5
-            
-            ans[i] -> max(left[i + k - 1] , right[i])
-        */
-        
-        int n = arr.length;
-        
-        int[] leftmax = new int[n];
-        for(int i=0; i<n; i++){
-            if(i % k == 0){
-                // i is the starting point of a box
-                leftmax[i] = arr[i];
+        for(int i=0; i<k; i++){
+            while(deque.size() > 0 && arr[i] >= arr[deque.getLast()]){
+                deque.removeLast();
             }
-            else{
-                leftmax[i] = Math.max(leftmax[i - 1] , arr[i]);
-            }
+
+            deque.addLast(i);
         }
-        
-        int[] rightmax = new int[n];
-        for(int i=n-1; i>=0; i--){
-            if((i + 1) % k == 0){
-                // i is the endpoint of a box
-                rightmax[i] = arr[i];
+
+        int[] ans = new int[arr.length - k + 1];
+
+        ans[0] = arr[deque.getFirst()];
+
+        for(int i=k; i<arr.length; i++){
+            while(deque.size() > 0 && deque.getFirst() <= i - k){
+                deque.removeFirst();
             }
-            else{
-                rightmax[i] = Math.max(arr[i] , i + 1 >= n ? Integer.MIN_VALUE : rightmax[i + 1]);
+
+            while(deque.size() > 0 && arr[i] >= arr[deque.getLast()]){
+                deque.removeLast();
             }
+
+            deque.addLast(i);
+
+            ans[i - k + 1] = arr[deque.getFirst()];
         }
-        
-        
-        int[] ans = new int[n - k + 1];
-        for(int i=0; i<ans.length; i++){
-            ans[i] = Math.max(rightmax[i] , leftmax[i + k - 1]);
-        }
+
         return ans;
     }
 }
