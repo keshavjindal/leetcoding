ae4db19d424963ce023993510737be05833b2ef0
keshavjindal <52865096+keshavjindal@users.noreply.github.com>
2024-03-02 21:30:14 -0500

    Time: 4 ms (91.65%) | Memory: 45.4 MB (90.06%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -10,55 +10,44 @@ class Solution {
             graph.add(new ArrayList<>());
         }
         
+        int[] indegree = new int[numCourses];
+        
         for(int[] e : prerequisites){
             int a = e[0];
             int b = e[1];
             
             // b -> a
             graph.get(b).add(a);
+            indegree[a] += 1;
         }
         
-        boolean[] visited = new boolean[numCourses];
-        boolean[] recStack = new boolean[numCourses];
-        ans = new int[numCourses];
-        idx = numCourses - 1;
-        cycleFound = 0;
+        // solve iteratively: kahn's algorithm
+        // bfs
+        int[] ans = new int[numCourses];
+        int idx = 0;
         
+        Queue<Integer> queue = new ArrayDeque<>();
         for(int i=0; i<numCourses; i++){
-            if(!visited[i]){
-                dfs(graph, visited, recStack, i);
-                if(cycleFound == 1){
-                    return new int[0];
+            if(indegree[i] == 0){
+                queue.add(i);
+            }
+        }
+        
+        while(queue.size() > 0){
+            int rem = queue.remove();
+            
+            ans[idx] = rem;
+            idx++;
+            
+            for(int nbr : graph.get(rem)){
+                indegree[nbr] -= 1;
+                if(indegree[nbr] == 0){
+                    queue.add(nbr);
                 }
             }
         }
         
-        return ans;
-    }
-    
-    public int[] ans;
-    public int idx;
-    public int cycleFound;
-    
-    public void dfs(ArrayList<ArrayList<Integer>> graph, boolean[] visited, boolean[] recStack, int src){
-        if(cycleFound == 1) return;
-        
-        visited[src] = true;
-        recStack[src] = true;
-        
-        for(int nbr : graph.get(src)){
-            if(visited[nbr] && recStack[nbr]){
-                cycleFound = 1;
-                return;
-            }
-            
-            if(!visited[nbr]){
-                dfs(graph, visited, recStack, nbr);
-            }
-        }
-        
-        recStack[src] = false;
-        ans[idx] = src;
-        idx--;
+        if(idx == numCourses) return ans;
+        else return new int[0];
     }
 }
