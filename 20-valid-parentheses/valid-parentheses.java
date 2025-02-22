324299565b9454daf41946bbf4d976ffe18d51e6
keshavjindal <52865096+keshavjindal@users.noreply.github.com>
2025-02-23 02:34:41 +0530

    Time: 2 ms (97.70%) | Memory: 41.9 MB (47.42%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -6,10 +6,13 @@ class Solution {
             char ch = s.charAt(i);
 
             if(ch == '(' || ch == '[' || ch == '{'){
+                // opening bracket -> push in stack
                 stack.push(ch);
             }
             else{
                 // closing bracket
+                // check stack top. if invalid, return false
+                // if valid. pop it and go to next character in string
                 if(ch == ')'){
                     if(stack.size() != 0 && stack.peek() == '('){
                         stack.pop();
