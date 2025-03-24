033eeac9cf6629e572c7433332ab8eb8c5bf33ce
Keshav Jindal <52865096+keshavjindal@users.noreply.github.com>
2025-03-24 01:15:47 -0400

    Time: 3 ms (86.33%) | Memory: 49.9 MB (39.28%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,33 +1,30 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        
         int ans = 0;
-        
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                char ch = grid[i][j];
-                
-                if(ch == '1'){
+
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[0].length; j++){
+                if(grid[i][j] == '1'){
                     ans++;
-                    dfs(grid, i, j);
+                    helper(grid, i, j);
                 }
             }
         }
-        
+
         return ans;
     }
-    
-    public void dfs(char[][] grid, int i, int j){
-        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length) return;
+
+    public void helper(char[][] grid, int i, int j){
+        if(i < 0 || j < 0) return;
+        if(i >= grid.length || j >= grid[0].length) return;
+
         if(grid[i][j] == '0') return;
-        
+
         grid[i][j] = '0';
-        
-        dfs(grid, i + 1, j);
-        dfs(grid, i, j + 1);
-        dfs(grid, i - 1, j);
-        dfs(grid, i, j - 1);
+
+        helper(grid, i + 1, j);
+        helper(grid, i, j + 1);
+        helper(grid, i, j - 1);
+        helper(grid, i - 1, j);
     }
 }
