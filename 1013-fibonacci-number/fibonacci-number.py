05a1033458ca8954fb94fbbf035df0e37e87115e
Keshav Jindal <52865096+keshavjindal@users.noreply.github.com>
2025-04-24 22:59:26 -0400

    Time: 33 ms (87.96%) | Memory: 17.6 MB (89.40%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        # tabulation
+        dp = [0 , 1]
+        i = 2
+        while i <= n:
+            dp.append(dp[i - 1] + dp[i - 2])
+            i += 1
+
+        return dp[n]
