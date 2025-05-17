24a13ccf9ca89384b47f5bad712d73acb94797ad
Keshav Jindal <52865096+keshavjindal@users.noreply.github.com>
2025-05-17 17:43:50 -0400

    Time: 0 ms (100.00%) | Memory: 41.7 MB (96.79%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -1,25 +1,24 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        // Approach: Two pointers: Subhesh Sir method
-        // Using Swapping
-        // Values of original elements is conserved
+        // Approach: Two pointers: Leetcode editorial
+        // Using reader - writer pointers
+        // Values of original elements is NOT conserved!
 
-        int i = 0;
-        int j = 0; // unknown
+        int reader = 0;
+        int writer = 0;
 
         int count = 0;
-        while(j < nums.length){
-            if(nums[j] == val){
-                j++;
-            }else{
+
+        while(reader < nums.length){
+            if(nums[reader] != val){
                 count++;
 
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
+                nums[writer] = nums[reader];
 
-                i++;
-                j++;
+                reader++;
+                writer++;
+            }else{
+                reader++;
             }
         }
 
