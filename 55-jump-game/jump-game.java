de6f52de30f816b7772d8a1b31f8d8e796952d08
Keshav Jindal <52865096+keshavjindal@users.noreply.github.com>
2025-05-18 12:34:05 -0400

    Time: 1 ms (99.88%) | Memory: 45.8 MB (55.95%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,25 +1,20 @@
 class Solution {
-    public boolean canJump(int[] nums) {
-        int[] dp = new int[nums.length + 10];
-        Arrays.fill(dp , -1);
-        return solve(nums , 0, dp);
-    }
+    public boolean canJump(int[] arr) {
+        // using greedy
+        // very good logic
+        // good index -> i is a good index if we can reach from i to last
+        // we need to find leftmost good index
+        // return leftmost_good_index == 0(start)
 
-    public boolean solve(int[] arr, int i, int[] dp){
-        if(i >= arr.length - 1) return true;
-        if(dp[i] != -1){
-            if(dp[i] == 0) return false;
-            else return true;
-        }
-        int jump = arr[i];
-        for(int idx=i+1; idx<=i+jump; idx++){
-            boolean temp = solve(arr , idx, dp);
-            if(temp == true){
-                dp[i] = 1;
-                return true;
+        int n = arr.length;
+        int leftMostGoodIndex = n - 1; // where we want to reach
+        for(int i=n-1; i>=0; i--){
+            if(i + arr[i] >= leftMostGoodIndex){
+                leftMostGoodIndex = i;
             }
         }
-        dp[i] = 0;
-        return false;
+
+        return leftMostGoodIndex == 0;
+
     }
 }
