ab04c7c5625a39da39eecf46196b0b51a9f17019
Keshav Jindal <52865096+keshavjindal@users.noreply.github.com>
2025-05-18 15:39:26 -0400

    Time: 1 ms (99.86%) | Memory: 47.3 MB (93.08%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -2,25 +2,29 @@ class Solution {
     public int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        
+
         dp = new int[m + 10][n + 10];
         for(int[] arr : dp){
             Arrays.fill(arr , -1);
         }
-        
-        return solve(grid, 0, 0);
+
+        return solve(grid , 0 , 0);
     }
-    
+
     public int[][] dp;
-    
-    public int solve(int[][] grid, int i, int j){
-        if(i == grid.length - 1 && j == grid[0].length - 1) return grid[i][j];
-        if(i == grid.length || j == grid[0].length) return Integer.MAX_VALUE;
+
+    // whats the min path sum if we go from (i,j)
+    // to bottom right, only using R(right) and D(down) steps
+    public int solve(int[][] matrix, int i, int j){
+        if(i >= matrix.length || j >= matrix[0].length) return Integer.MAX_VALUE;
+
         if(dp[i][j] != -1) return dp[i][j];
-        
-        int rr1 = solve(grid, i, j + 1);
-        int rr2 = solve(grid, i + 1, j);
-        
-        return dp[i][j] = grid[i][j] + Math.min(rr1 , rr2);
+
+        int rr1 = solve(matrix, i, j + 1); // go right
+        int rr2 = solve(matrix, i + 1, j); // go down
+
+        int better = Math.min(rr1 , rr2);
+        if(better == Integer.MAX_VALUE) better = 0;
+        return dp[i][j] = matrix[i][j] + better;
     }
 }
