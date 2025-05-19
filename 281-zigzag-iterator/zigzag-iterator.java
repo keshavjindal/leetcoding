c31140007426bee329c0eef12408c7aedb101729
Keshav Jindal <52865096+keshavjindal@users.noreply.github.com>
2025-05-19 18:15:14 -0400

    Time: 1 ms (100.00%) | Memory: 45.1 MB (26.59%) - LeetSync

--- 281-zigzag-iterator/zigzag-iterator.java
+++ 281-zigzag-iterator/zigzag-iterator.java
@@ -1,52 +1,50 @@
 public class ZigzagIterator {
-    int one;
-    int two;
-    List<Integer> list1;
-    List<Integer> list2;
-    boolean fromOne;
-    
+    private int current;
+  
+    // Total number of vectors
+    private int listCount;
+  
+    // List to keep track of the indexes of current read position for each vector.
+    private List<Integer> indices = new ArrayList<>();
+  
+    // List holding the two input vectors.
+    private List<List<Integer>> vectors = new ArrayList<>();
 
+    // Constructor which initializes the ZigzagIterator with two lists (v1 and v2).
     public ZigzagIterator(List<Integer> v1, List<Integer> v2) {
-        list1 = v1;
-        one = 0;
-        list2 = v2;
-        two = 0;
-        fromOne = true;
+        current = 0;
+        listCount = 2; // Since we always have two lists (v1 and v2) for this iterator.
+        indices.add(0);  // Adding initial indices for both vectors.
+        indices.add(0);
+        vectors.add(v1); // Adding the provided lists to the vectors list.
+        vectors.add(v2);
     }
 
+    // Returns the next element in the zigzag iteration.
     public int next() {
-        int eleToReturn;
+        // Get the current vector and the index for the element to return.
+        List<Integer> vector = vectors.get(current);
+        int index = indices.get(current);
+        int result = vector.get(index); // Get the next element.
+        indices.set(current, index + 1); // Update the index for the current vector.
+        current = (current + 1) % listCount; // Move to the next vector.
+        return result;
+    }
 
-        if(fromOne){
-            fromOne = false;
-            // return from first list
-            if(one < list1.size()){
-                eleToReturn = list1.get(one);
-                one++;
-            }
-            else{
-                eleToReturn = list2.get(two);
-                two++;
-            }
-        }else{
-            fromOne = true;
-            // return from second list
-            if(two < list2.size()){
-                eleToReturn = list2.get(two);
-                two++;
-            }
-            else{
-                eleToReturn = list1.get(one);
-                one++;
+    // Returns true if there is a next element in the iteration, false otherwise.
+    public boolean hasNext() {
+        int start = current; // Remember the starting point.
+        // Cycle through the vectors to find if any vector still has elements left.
+        while (indices.get(current) == vectors.get(current).size()) {
+            current = (current + 1) % listCount; // Move to next vector.
+            if (start == current) {
+                // If we have cycled through all vectors without finding an element,
+                // then there are no elements left, return false.
+                return false;
             }
         }
-
-        return eleToReturn;
-    }
-
-    public boolean hasNext() {
-        if(one + two == list1.size() + list2.size()) return false;
-        else return true;
+        // If we have found a vector that still has elements, return true.
+        return true;
     }
 }
 
