43fccb86fe566f5014a5fafae3928e0c7850cc47
Keshav Jindal <52865096+keshavjindal@users.noreply.github.com>
2025-05-29 20:42:50 -0400

    Time: 1 ms (100.00%) | Memory: 45.3 MB (7.77%) - LeetSync

--- 281-zigzag-iterator/zigzag-iterator.java
+++ 281-zigzag-iterator/zigzag-iterator.java
@@ -1,58 +1,21 @@
 public class ZigzagIterator {
-    private int current;
-    private int totalElements;
-    private int returnedElements;
-  
-    // Total number of vectors
-    private int listCount;
-  
-    // List to keep track of the indexes of current read position for each vector.
-    private List<Integer> indices = new ArrayList<>();
-  
-    // List holding the two input vectors.
-    private List<List<Integer>> vectors = new ArrayList<>();
+    Queue<Iterator> queue;
 
-    // Constructor which initializes the ZigzagIterator with two lists (v1 and v2).
     public ZigzagIterator(List<Integer> v1, List<Integer> v2) {
-        current = 0;
-        listCount = 2; // Since we always have two lists (v1 and v2) for this iterator.
-        indices.add(0);  // Adding initial indices for both vectors.
-        indices.add(0);
-        vectors.add(v1); // Adding the provided lists to the vectors list.
-        vectors.add(v2);
-        totalElements = v1.size() + v2.size();
-        returnedElements = 0;
+        queue = new LinkedList<>();
+        if(v1.size() != 0) queue.add(v1.iterator());
+        if(v2.size() != 0) queue.add(v2.iterator());
     }
 
-    // Returns the next element in the zigzag iteration.
     public int next() {
-        // Get the current vector and the index for the element to return.
-        List<Integer> vector = vectors.get(current);
-        int index = indices.get(current);
-        int result = vector.get(index); // Get the next element.
-        indices.set(current, index + 1); // Update the index for the current vector.
-        current = (current + 1) % listCount; // Move to the next vector.
-        returnedElements++;
-        return result;
+        Iterator rem = queue.remove();
+        int res = (int)rem.next();
+        if(rem.hasNext()) queue.add(rem);
+        return res;
     }
 
-
-    // Returns true if there is a next element in the iteration, false otherwise.
     public boolean hasNext() {
-        int start = current; // Remember the starting point.
-        // Cycle through the vectors to find if any vector still has elements left.
-        while (indices.get(current) == vectors.get(current).size()) {
-            current = (current + 1) % listCount; // Move to next vector.
-            if (start == current) {
-                // If we have cycled through all vectors without finding an element,
-                // then there are no elements left, return false.
-                return false;
-            }
-        }
-        // If we have found a vector that still has elements, return true.
-        return true;
-
-        // return returnedElements < totalElements;
+        return queue.size() != 0;
     }
 }
 
